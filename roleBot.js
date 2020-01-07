af98105ef97cdef3935a8793edc3ee414a61381a
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-01-07 16:43:41 +1100

    Finished Google API setup for Google Sheets
    Finished setting up an Auth2 for Google Sheets API

--- roleBot.js
+++ roleBot.js
@@ -1,9 +1,28 @@
+
+
+/*
+    Discord includes
+ */
 const snekfetch = require('snekfetch');
 const discord = require('discord.js');
 const client = new discord.Client({
     partials: ['MESSAGE']
 });
+
+/*
+    CSV Includes
+ */
 const csv = require('fast-csv');
+
+/*
+    Google Includes
+ */
+const readline = require('readline');
+const {google} = require('googleapis');
+
+/*
+    Discord Logins
+ */
 const {prefix} = require('./config.json');
 const token = process.env.token;
 client.login(token);
@@ -14,6 +33,66 @@ client.on('ready', () => {
 let channel_id = "650567782890471436";
 let message_id = "651598494141775872";
 
+const SCOPES = ['https://www.googleapis.com/auth/spreadhseets.readonly'];
+const TOKEN_PATH = 'token.json';
+
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file: ', err);
+    authorize(JSON.parse(content), listMajors);
+});
+
+function authorize(credentials, callback){
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getNewToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials((JSON.parse(token)));
+        callback(oAuth2Client);
+    });
+}
+
+function getNewToken(oAuth2Client, callback) {
+    const authURL = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorise this app by visiting this url: ', authURL);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error while trying to retrieve access token', err);
+        oAuth2Client.setCredentials(token);
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+            if (err) return console.error(err);
+            console.log('Token stored to ', TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+    });
+}
+
+function listMajors(auth) {
+    const sheets = google.sheets({version: 'v4', auth});
+    sheets.spreadsheets.value.get({
+        spreadsheetId: '17QRSyBhAvjp6GIXGcKki16jj8F6HhmC8Nt0YPyRqhPI',
+        range: 'Class Data ! A2:E',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const rows = res.data.values;
+        if (rows.length) {
+            console.log('Name, Major: ');
+            rows.map((row)=> {
+                console.log(`${row[0]}, ${row[4]}`);
+            });
+        } else {
+            console.log('No data found');
+        }
+
+    });
+    }
 
 
 client.on("ready", (reaction, user) => {
