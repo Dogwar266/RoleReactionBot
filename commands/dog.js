4a2f87b08d4a02c9d92c5730c6b708a6f2a62efc
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-02-20 19:04:11 +1100

    Attempting another method of fetching a random dog

--- commands/dog.js
+++ commands/dog.js
@@ -1,32 +1,3 @@
-function image(message) {
-    let options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + "dog",
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    }
-}
-request (options, function(error, response, responseBody){
-    if (error){
-        return;
-    }
-    let $ = cheerio.load(responseBody)
-
-    let links = $(".image a.link");
-
-    let urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-
-    console.log(urls);
-
-    if (!urls.length) {
-        return;
-    }
-
-
-});
-
 
 module.exports = {
 
@@ -35,7 +6,13 @@ module.exports = {
     guildOnly: true,
 
     execute(message, args) {
-
-        message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
+        try {
+            get('https://random.dog/woof').then(response => {
+                message.channel.send({files: [{attachment: response.body.file, name: `dog.${response.body.file.split('.')[2]}`}]});
+                console.log('random dog picture');
+            })
+        } catch (e) {
+            console.log('error!');
+        }
     }
-};
+}
