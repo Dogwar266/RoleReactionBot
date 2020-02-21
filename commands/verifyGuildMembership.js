5177212a3a62676627f2f21e018e31d5844f98fb
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-02-21 19:30:21 +1100

    Started initial work on guild verification

---
+++ commands/verifyGuildMembership.js
@@ -0,0 +1,29 @@
+const api = "https://api.guildwars2.com/v2/guild/F5BF2DF0-A119-E911-81AA-D66D0E22CAB6/members?access_token=70428CD7-F1EA-6642-A4AF-A6F28396D184E753B2A8-9618-40F1-82DB-8DFF2118F29D";
+const snekfetch = require("snekfetch");
+const Discord = require("discord.js");
+
+
+module.exports = {
+
+    name: 'verify',
+    description: 'Verify your Discord account with your GW2 ingame Account',
+    guildOnly: true,
+
+    execute(message, args){
+        const ingameName = parse(args[0]);
+        
+        try {
+            snekfetch.get(api).then(r => {
+                let memberArray = JSON.parse(JSON.stringify(r.body));
+                for (let i = 0; i <memberArray.length; i++){
+                    console.log(memberArray[i].name);
+                }
+            });
+        } catch (e){
+            console.log(e);
+        }
+
+    }
+
+
+}
