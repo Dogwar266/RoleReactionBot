74507f46eb568baa3fa91d6e0229764113c6adc9
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-02-21 13:44:08 +1100

    Added a meme function

---
+++ commands/abree.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    name: 'SafeFromAbreesShaft',
+    description : 'Lists all members safe from Abrees shaft',
+
+    execute(message, args) {
+        message.channel.send("Anyone with a vagina");
+    }
+
+};
