3e40ab6ad72e3c0ba6e24c5434d6006ed8e2d460
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-02-21 13:51:39 +1100

    Debugging Meme Function

--- commands/abree.js
+++ commands/abree.js
@@ -4,6 +4,10 @@ module.exports = {
     description : 'Lists all members safe from Abrees shaft',
 
     execute(message) {
-        message.channel.send('Anyone with a vagina');
+        try {
+            message.channel.send('Anyone with a vagina');
+        } catch (e){
+            console.log(e);
+        }
     },
 };
