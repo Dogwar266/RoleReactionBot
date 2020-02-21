52cf11b5161255b9c39df540d408fe28776b4de0
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-02-21 22:04:48 +1100

    Added a message reply

--- commands/verifyGuildMembership.js
+++ commands/verifyGuildMembership.js
@@ -23,6 +23,7 @@ module.exports = {
                     console.log(memberArray[i].name);
                     if (ingameName === memberArray[i].name){
                          member.addRole(role).catch(console.error);
+                         message.reply('You are in the guild!')
                     }
                 }
             });
