b175485b1780140a167635114ce20d56574c2d71
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-02-23 20:01:10 +1100

    Initial menu setup for market command

---
+++ commands/market.js
@@ -0,0 +1,44 @@
+const snekfetch = require("snekfetch");
+const Discord = require("discord.js");
+
+
+
+module.exports = {
+
+  name: "market",
+  description: "Display various GW2 Market information",
+  guildOnly: true,
+
+  execute(message, args){
+
+      let choice = 0;
+
+      do {
+
+          message.channel.send('**WELCOME TO THE MARKET COMMAND**\n -------------------------\nPlease choose an option from the menu below!1. Buy Price of an Item\n 2. Sell Price of an Item\n 3. Stack Price of an Item\n 4. Custom Quantity Price of an Item\n 5. Exit');
+
+          switch (choice){
+              case 1:
+                  break;
+              case 2:
+                  break;
+              case 3:
+                  break;
+              case 4:
+                  break;
+              case 5:
+                  break;
+              default:
+                  break;
+          }
+
+      }while (true)
+
+
+
+  }
+
+
+
+
+};
