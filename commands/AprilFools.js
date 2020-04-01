ce81532aba0d1ea9b5d99b22e5714e668129aa32
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-04-01 12:55:36 +1100

    Revert commit

--- commands/AprilFools.js
+++ commands/AprilFools.js
@@ -12,8 +12,13 @@ module.exports = {
         {
             console.log('I got here');
             try {
+<<<<<<< HEAD
                 for (let i = 0; i < 15; i++) {
                     message.channel.send('@everyone');
+=======
+                for (var i = 0; i < 15; i++) {
+                    message.channel.send("@everyone");
+>>>>>>> parent of b9de218... changed var to let
                     await sleep(5000);
                 }
             } catch (e) {
