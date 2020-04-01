c561c26e56b35a03d39b8f66f35ab1337038fde1
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-04-01 13:19:39 +1100

    Removed functions

--- commands/AprilFools.js
+++ commands/AprilFools.js
@@ -5,25 +5,18 @@ module.exports = {
     name: 'aprilFools',
     description : 'One time only command',
 
+    execute(message) {
 
-    execute(message)
-    {
-        async function aprilFoolsJoke()
-        {
-            console.log('I got here');
-            try {
-
-                for (let i = 0; i < 15; i++) {
-                    message.channel.send('@everyone');
-                }
-                    for (let i = 0; i < 15; i++) {
-                        message.channel.send("@everyone");
-                        await sleep(5000);
-                    }
-                } catch (e){
-                console.log(e);
+        console.log('I got here');
+        try {
+            for (let i = 0; i < 15; i++) {
+                message.channel.send('@everyone');
             }
+            for (let i = 0; i < 15; i++) {
+                message.channel.send("@everyone");
+            }
+        } catch (e) {
+            console.log(e);
         }
-        aprilFoolsJoke();
     }
 };
